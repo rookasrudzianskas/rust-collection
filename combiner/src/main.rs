e4c488d0dac8067ad84dd723e96e8f25e5dbccd3
Rokas <rookas.rudzenskas@gmail.com>
2022-11-08 18:02:15 +0200

    Rokas

--- combiner/src/main.rs
+++ combiner/src/main.rs
@@ -4,6 +4,7 @@ use image::{ io::Reader, DynamicImage, ImageFormat, imageops::FilterType::Triang
 use std::{ io::BufReader, fs::File };
 use std::convert::TryInto;
 
+#[derive(Debug)]
 enum ImageDataErrors {
     DifferentImageFormats,
 }
@@ -38,6 +39,7 @@ fn main() -> Result<(), ImageDataErrors> {
     }
 
     let (image_1, image_2) = standardise_size(image_1, image_2);
+    let output = FloatingImage::new(image_1.width, image_1.height, args.output);
     ok(())
 }
 
